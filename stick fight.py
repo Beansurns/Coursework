2253d73d36f5144900969cacbcdd00aabe2161ab
Beansurns <100275366+Beansurns@users.noreply.github.com>
2022-03-03 12:17:33 +0000

    stick fight file

---
+++ stick fight.py
@@ -0,0 +1,54 @@
+import pygame
+from random import randint as rand
+
+pygame.init()
+s_width, s_height = 1900, 950
+screen = pygame.display.set_mode([s_width,s_height])
+
+class Blocks:
+    def __init__(self, width=10, height=10, pos=(1900,0), colour=(255,255,255)):
+        self.width = width
+        self.heihgt = height
+        self.pos = pos
+        self.colour = colour
+        
+    def update(self):
+        
+        self.draw()
+        
+    def draw(self):
+        pygame.Rect(self.pos[0], self.pos[1], self.width, self.height)
+        
+
+class Players:
+    def __init__(self, width=10, height=20, pos=(1900,0), colour=(255,255,255)):
+        self.width = width
+        self.height = height
+        self.pos = pos
+        self.colour = colour
+        
+    def update():
+        pass
+    
+    def draw():
+        pass
+    
+    
+
+
+
+
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill((43, 45, 47))
+    pygame.display.flip()
+
+            
+            
+pygame.quit()
