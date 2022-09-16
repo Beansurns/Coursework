609411632cc73d85af32084e8e4821e69c31f41f
seabur0809 <seabur0809@gmail.com>
2022-09-16 16:00:00 +0100

    moovement

--- stick fight.py
+++ stick fight.py
@@ -5,11 +5,12 @@ pygame.init()
 #setting variables
 s_width, s_height = 1900, 950
 screen = pygame.display.set_mode([s_width,s_height])
-g = 0.02
+g = 9.8
 ydrag = 0.8
 xdrag = 0.5
 drag = 0.6
 xvdrag = 0.1/0.13
+clock = pygame.time.Clock()
 
 class Blocks:
     def __init__(self, width=10, height=10, pos=(1900,0), colour=(255,255,255)):
@@ -18,7 +19,7 @@ class Blocks:
         self.pos = pos
         self.colour = colour
         
-    def update(self):
+    def update(self, dt):
         self.draw()
         
     def draw(self):
@@ -57,36 +58,35 @@ class Players:
         self.keyleft = keyleft
         self.keyright = keyright
 
-    def update(self):
-        self.yacc += g
+    def update(self, dt):
         self.yacc = self.yacc * ydrag
-        self.yvel += self.yacc
+        self.yvel += (self.yacc + g)*dt
         self.xacc = self.xacc * xdrag
-        self.xvel += self.xacc
+        self.xvel += self.xacc*dt
         self.xvel = self.xvel * xvdrag
 
 
-        self.pos = (self.pos[0]+self.xvel,self.pos[1]+self.yvel)
+        self.pos = (self.pos[0]+(self.xvel * dt), self.pos[1]+(self.yvel * dt))
         #Collision detection between players and surfaces
         for border in borders:
             if border.pos[1] == 931 and -1 < (self.pos[1] + self.radius) - border.pos[1]:
                 self.pos = (self.pos[0], border.pos[1] - self.radius)
                 self.yvel = 0
                 self.ground = True
-            elif border.vert == False and border.tr == True and -1 < (self.pos[1] + self.radius) - border.pos[1] < 10 and \
+            elif not border.vert and border.tr and -1 < (self.pos[1] + self.radius) - border.pos[1] < 10 and \
                     (self.pos[0] > border.pos[0] and self.pos[0] < (border.pos[0] + border.width)):
                 self.pos = (self.pos[0], border.pos[1]-self.radius)
                 self.yvel = 0
                 self.ground = True
-            elif border.vert == True and border.tr == True and -1 < ((self.pos[0] - self.radius) - border.pos[0]) < 1 \
+            elif border.vert and border.tr and -1 < ((self.pos[0] - self.radius) - border.pos[0]) < 1 \
                     and (self.pos[1] > border.pos[1] and self.pos[1] < (border.pos[1] + border.height)):
                 self.xvel = 0
                 self.pos = (border.pos[0] + border.width + self.radius, self.pos[1])
-            elif border.vert == True and border.tr == False and -1 < ((self.pos[0] + self.radius) - border.pos[0]) < 1 \
+            elif border.vert and not border.tr and -1 < ((self.pos[0] + self.radius) - border.pos[0]) < 1 \
                     and (self.pos[1] > border.pos[1] and self.pos[1] < (border.pos[1] + border.height)):
                 self.xvel = 0
                 self.pos = (border.pos[0] - border.width - self.radius, self.pos[1])
-            elif border.vert == False and border.tr == False and -10 < (self.pos[1] - self.radius) - border.pos[1] < 1 and \
+            elif not border.vert and not border.tr and -10 < (self.pos[1] - self.radius) - border.pos[1] < 1 and \
                     (self.pos[0] > border.pos[0] and self.pos[0] < (border.pos[0] + border.width)):
                 self.pos = (self.pos[0], border.pos[1] + self.radius + border.height)
                 self.yvel = 0
@@ -94,14 +94,14 @@ class Players:
         #movement
         keys = pygame.key.get_pressed()
         if keys[self.keyleft]:
-            self.xacc += -0.8
+            self.xacc += -100
         if keys[self.keyright]:
-            self.xacc += 0.8
+            self.xacc += 100
         if keys[self.keyup] and self.ground:
-            self.yacc += -1.3
+            self.yacc += -0.3
             self.ground = False
         if keys[self.keydown]:
-            self.yacc += 0.3
+            self.yacc += 0.1
         self.draw()
     
     def draw(self):
@@ -119,15 +119,15 @@ players = [Players(30, (1100, 475), (2, 148, 165),num = 0,keyup = pygame.K_UP,ke
 blocks = [Blocks(1900, 20, (0,930), (255,255,255)), Blocks(20, 950, (0,0), (255,255,255)), Blocks(20, 950, (1880,0),
                 (255,255,255)), Blocks(800, 20, (550,630), (255,255,255)), Blocks(400, 20, (750,330), (255,255,255))]
 
-borders = [Borders(1900,1,(0,931), False, True), Borders(1, 950, (19,0), True, True),
-           Borders(1, 950, (1880,0), True, False), Borders(800,1,(550,631), False, True),
-           Borders(1, 20, (1349,630), True, True), Borders(1, 20, (550,630), True, False),
-           Borders(800,1,(550,650), False, False), Borders(400,1,(750,331), False, True),
-           Borders(1, 20, (1149,330), True, True), Borders(1, 20, (750,330), True, False),
-           Borders(800,1,(750,350), False, False)]
+borders = [Borders(1900, 1, (0, 931), False, True), Borders(1, 950, (19, 0), True, True),
+           Borders(1, 950, (1880, 0), True, False), Borders(800, 1, (550, 631), False, True),
+           Borders(1, 20, (1349, 630), True, True), Borders(1, 20, (550, 630), True, False),
+           Borders(800, 1, (550, 650), False, False), Borders(400, 1, (750, 331), False, True),
+           Borders(1, 20, (1149, 330), True, True), Borders(1, 20, (750, 330), True, False),
+           Borders(400, 1, (750, 350), False, False), Borders(1900, 1, (0, 0), False, False)]
 running = True
 while running:
-    
+    dt = clock.tick()/1000
     for event in pygame.event.get():
         
         if event.type == pygame.QUIT:
@@ -135,9 +135,9 @@ while running:
 
     screen.fill((43, 45, 47))
     for block in blocks:
-        block.update()
+        block.update(dt)
     for player in players:
-        player.update()
+        player.update(dt)
     pygame.display.flip()
 
             
